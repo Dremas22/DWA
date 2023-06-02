4c0758bce456230feab9b5f95dfc8d3596f489e2
Tshepo Masilo <76808634+Dremas22@users.noreply.github.com>
2023-06-02 16:24:55 +0200

    saving changes

--- DWA5/script.js
+++ DWA5/script.js
@@ -10,14 +10,18 @@ form.addEventListener("submit", (event) => {
     const entries = new FormData(event.target);
     const { dividend, divider } = Object.fromEntries(entries);
 
+    //------VALIDATING INPUT DATA--------------//
+
     if (dividend < 0 || divider < 0) {
         showResult.innerText = 'Division not performed. Invalid number provided. Try again.';
 
     }
+
     else if (dividend > 0 && divider > 0) {
         let answer = dividend / divider;
         showResult.innerText = Math.floor((answer / 10) * 10);
     }
+
     else if (dividend != Number || divider != Number) {
         try {
 
@@ -25,15 +29,14 @@ form.addEventListener("submit", (event) => {
                 throw new Error("No entries found");
             }
 
-            // Rest of your code for performing calculations
-
         } catch (error) {
-            // Handle the error gracefully
+            // Handling the error
             console.error("An error occurred:", error);
             showResult.innerText = 'No calculation performed';
             alert("An error occurred. Please try again later.");
         }
     }
+
     window.addEventListener('submit', () => {
         if (dividend === 'YOLO' && divider === '+++') { 
            document.body.innerHTML = '<h1>Something critical went wrong. Please reload the page</h1>';
